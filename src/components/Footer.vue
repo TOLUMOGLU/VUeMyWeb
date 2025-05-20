3ecc4ac5648a66f6c12003445451baeee2477c74
Azime Tolumoğlu <azimet@bilge.com.tr>
2025-05-20 17:05:52 +0300

    header mıobil and footer somp add.

---
+++ src/components/Footer.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-footer
+    color="white"
+    app
+    class="pa-6"
+  >
+    <v-row
+      justify="space-between"
+      class="text-left"
+      style="width: 100%"
+      no-gutters
+    >
+      <v-col cols="12" md="6" class="footer-left">
+        <div>© 2035 by Maya Nelson.</div>
+        <div>Powered and secured by <a href="https://www.wix.com" target="_blank" rel="noopener">Wix</a></div>
+      </v-col>
+
+      <v-col cols="12" md="1" class="footer-center">
+        <div><strong>Call</strong></div>
+        <div>123-456-7890</div>
+      </v-col>
+
+      <v-col cols="12" md="1" class="footer-center">
+        <div><strong>Write</strong></div>
+        <div><a href="mailto:info@mysite.com">info@mysite.com</a></div>
+      </v-col>
+
+      <v-col cols="12" md="2" class="footer-right d-flex align-center justify-end">
+        <v-btn icon href="https://facebook.com" target="_blank" rel="noopener">
+          <v-icon>mdi-facebook</v-icon>
+        </v-btn>
+        <v-btn icon href="https://twitter.com" target="_blank" rel="noopener">
+          <v-icon>mdi-twitter</v-icon>
+        </v-btn>
+        <v-btn icon href="https://linkedin.com" target="_blank" rel="noopener">
+          <v-icon>mdi-linkedin</v-icon>
+        </v-btn>
+        <v-btn icon href="https://instagram.com" target="_blank" rel="noopener">
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.footer-left {
+  font-size: 14px;
+  font-style: italic;
+  color: #333;
+}
+
+.footer-center {
+  font-size: 14px;
+  color: #333;
+  text-align: center;
+
+}
+
+.footer-center strong {
+  display: block;
+  margin-bottom: 4px;
+}
+
+.footer-right v-btn {
+  margin-left: 8px;
+  color: #333;
+}
+
+.footer-right v-btn:hover {
+  color: #673ab7;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,65 @@
 <template>
-  <v-app-bar app color="white" class="pa-4">
-    <div class="title-container pa-10">
-      <span class="name">Azime Tolumoğlu</span>
-      <span class="subtitle">/ SOFTWARE ENGINEER</span>
-    </div>
+  <v-container fluid>
+    <v-app-bar app color="white" class="pa-4">
+      <v-row class="align-center" style="width: 100%" no-gutters>
+        <v-col cols="8" md="4" class="d-flex align-center">
+          <div class="title-container">
+            <span class="name">Azime Tolumoğlu</span>
+            <span class="subtitle">/ SOFTWARE ENGINEER</span>
+          </div>
+        </v-col>
 
-    <v-spacer></v-spacer>
+        <v-col
+          cols="4"
+          md="8"
+          class="d-none d-md-flex justify-end align-center"
+        >
+          <v-btn text class="custom-btn" @click="$emit('navigate', 'about-me')">ABOUT ME</v-btn>
+          <v-btn text class="custom-btn" @click="$emit('navigate', 'resume')">RESUME</v-btn>
+          <v-btn text class="custom-btn" @click="$emit('navigate', 'projects')">PROJECTS</v-btn>
+          <v-btn text class="custom-btn" @click="$emit('navigate', 'contact')">CONTACT</v-btn>
+        </v-col>
 
-    <v-btn text class="custom-btn" @click="$emit('navigate', 'about-me')">ABOUT ME</v-btn>
-    <v-btn text class="custom-btn" @click="$emit('navigate', 'resume')">RESUME</v-btn>
-    <v-btn text class="custom-btn" @click="$emit('navigate', 'projects')">PROJECTS</v-btn>
-    <v-btn text class="custom-btn" @click="$emit('navigate', 'contact')">CONTACT</v-btn>
-  </v-app-bar>
+        <v-col
+          cols="4"
+          class="d-flex d-md-none justify-end align-center"
+        >
+          <v-btn icon @click="drawer = true">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      location="right"
+      class="d-md-none"
+    >
+      <v-list>
+        <v-list-item @click="$emit('navigate', 'about-me'); drawer = false">
+          <v-list-item-title>ABOUT ME</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="$emit('navigate', 'resume'); drawer = false">
+          <v-list-item-title>RESUME</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="$emit('navigate', 'projects'); drawer = false">
+          <v-list-item-title>PROJECTS</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="$emit('navigate', 'contact'); drawer = false">
+          <v-list-item-title>CONTACT</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const drawer = ref(false)
+</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
@@ -26,13 +73,13 @@
 
 .name {
   font-size: 24px;
-  font-weight: 600; 
+  font-weight: 600;
   color: black;
 }
 
 .subtitle {
   font-size: 16px;
-  font-weight: 400; 
+  font-weight: 400;
   color: black;
   margin-left: 8px;
   font-style: italic;
@@ -61,4 +108,8 @@
   outline: none !important;
   border: none !important;
 }
+.v-navigation-drawer {
+  padding-top: 64px !important;
+}
+
 </style>
